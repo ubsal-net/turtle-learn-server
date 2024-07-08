82af362791a3378139b71c82f92b224770569e59
Jeongmu <Kang815595@gmail.com>
2024-07-08 16:45:59 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/dsu/turtlelearnserver/TurtleLearnServerApplication.java
@@ -0,0 +1,13 @@
+package com.dsu.turtlelearnserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleLearnServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtleLearnServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsu/turtlelearnserver/TurtleLearnServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsu.turtlelearnserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtleLearnServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
