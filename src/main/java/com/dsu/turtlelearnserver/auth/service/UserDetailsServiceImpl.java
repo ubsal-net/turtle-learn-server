51c694405929fc312692b24a577d52191f463458
Jeongmu <Kang815595@gmail.com>
2024-07-16 17:26:40 +0900

    feat: UserDetail 관련 기능 구현

---
+++ src/main/java/com/dsu/turtlelearnserver/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.dsu.turtlelearnserver.auth.service;
+
+import com.dsu.turtlelearnserver.auth.util.UserDetailsImpl;
+import com.dsu.turtlelearnserver.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUsername(username)
+            .map(UserDetailsImpl::new)
+            .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다 : " + username));
+    }
+}
